585afb8dda1ed5bd567746e29e14e950ab9e7fe3
Alex Na <alexn@lexansoft.com>
2025-08-13 14:26:19 -0700

    chore: release v1.2

---
+++ src/version.ts
@@ -0,0 +1,5 @@
+// src/version.ts
+/** App version in the format 1.x (minor auto-bumps on `npm run deploy`) */
+export const APP_VERSION = "1.2";
+export const APP_GIT_TAG = `v${APP_VERSION}`;
+export default APP_VERSION;
